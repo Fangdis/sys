ac25022b6178fd24e263fd9c25bc89ce85f2cfa4
Fangdis <574701828@qq.com>
2019-09-30 17:36:17 +0800

    init

---
+++ sys-server/src/main/java/com/fangdi/sys/SysServerApplication.java
@@ -0,0 +1,13 @@
+package com.fangdi.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysServerApplication.class, args);
+	}
+
+}
